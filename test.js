264ae9ec1fd145c7544b227ab6f954368557d6f1
vishveshrathore20 <vishveshrathore20@gmail.com>
2024-10-03 13:52:11 +0530

    APIs in javascript

--- test.js
+++ test.js
@@ -1,1 +1,26 @@
 console.log("Vishvesh");
+
+{
+  const a = 10;
+  console.log(a);
+  let b = 20;
+  console.log(b);
+  if (true) {
+    const c = 30;
+    console.log(c);
+    let d = 40;
+
+    console.log(d);
+
+    console.log(a);
+  }
+  console.log(b);
+
+  let e = 50;
+  console.log(e);
+  console.log(a);
+
+  function f() {
+    console.log("Inside function f");
+  }
+}
